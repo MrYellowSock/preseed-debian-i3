645700bff89b64b5c1eede9c803df2f7ecfb3797
MrYellowSock <hahaunji@gmail.com>
2023-04-10 19:42:19 +0700

    fix

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # I3 + essentials installation
 apt update
 apt -y install wget curl
@@ -10,8 +11,11 @@ apt -y install i3 xorg xinit lightdm thunar neovim xrdp xfce4-terminal diodon ht
 su - ss
 cd ~
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source .bashrc
 nvm install --lts
-mkdir -p ~JobCredentials/con-release/
+mkdir -p ~/JobCredentials/con-release/
+mkdir -p ~/.config/i3/
+wget https://raw.githubusercontent.com/MrYellowSock/preseed-debian-i3/master/config -O ~/.config/i3/config
 
 # enabling i3 desktop
 systemctl enable lightdm
