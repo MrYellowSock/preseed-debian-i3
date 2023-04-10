6c3d4de3e84ac7e7b3ee582f7221432ccc17224f
MrYellowSock <hahaunji@gmail.com>
2023-04-10 21:18:29 +0700

    more

--- postinstall.sh
+++ postinstall.sh
@@ -7,18 +7,10 @@ sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
 apt update
 apt -y install i3 xorg xinit lightdm thunar neovim xrdp xfce4-terminal diodon htop google-chrome-stable
 
-# main user's utility
-su - ss
-cd ~
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-source .bashrc
-nvm install --lts
-mkdir -p ~/JobCredentials/con-release/
-mkdir -p ~/.config/i3/
-wget https://raw.githubusercontent.com/MrYellowSock/preseed-debian-i3/master/config -O ~/.config/i3/config
-
 # enabling i3 desktop
 systemctl enable lightdm
 systemctl enable lightdm
 systemctl enable graphical.target --force
 systemctl set-default graphical.target
+
+su - ss -c "curl https://raw.githubusercontent.com/MrYellowSock/preseed-debian-i3/master/postinstall_user.sh | bash"
