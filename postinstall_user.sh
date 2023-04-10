eefe2786fdb17a5fb93d4254d612990de4c45d10
MrYellowSock <hahaunji@gmail.com>
2023-04-10 21:25:30 +0700

    fix

--- postinstall_user.sh
+++ postinstall_user.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # main user's utility
-su - ss
-cd ~
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+curl https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 source .bashrc
 nvm install --lts
 mkdir -p ~/JobCredentials/con-release/
