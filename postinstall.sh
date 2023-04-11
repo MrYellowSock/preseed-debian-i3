170de9eef4d221b77868763adad74e1b4699e888
MrYellowSock <hahaunji@gmail.com>
2023-04-11 15:45:28 +0700

    add dummy display

--- postinstall.sh
+++ postinstall.sh
@@ -4,12 +4,18 @@ apt-get update
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 apt-get update
-apt-get -y install i3 xorg xinit lightdm thunar neovim xrdp xfce4-terminal fonts-thai-tlwg podman diodon htop google-chrome-stable
+apt-get -y install i3 xorg xinit lightdm thunar neovim git xrdp xfce4-terminal fonts-thai-tlwg podman diodon htop google-chrome-stable
 
 # enabling i3 desktop
 systemctl enable lightdm
 systemctl enable lightdm
 systemctl enable graphical.target --force
 systemctl set-default graphical.target
+
+# create dummy display for OS to launch
+git clone https://github.com/dianariyanto/virtual-display-linux.git
+cd virtual-display-linux
+chmod +x vdl-monitor
+./vdl-monitor
 
 su - ss -c "curl https://raw.githubusercontent.com/MrYellowSock/preseed-debian-i3/master/postinstall_user.sh | bash"
