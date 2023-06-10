2e28482718030d954bdfb73385c10b813fae48d6
MrYellowSock <hahaunji@gmail.com>
2023-06-10 22:35:39 +0700

    more apckgga

--- postinstall.sh
+++ postinstall.sh
@@ -4,7 +4,7 @@ apt-get update
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 apt-get update
-apt-get -y install i3 xorg xinit lightdm thunar neovim git xrdp xfce4-terminal fonts-thai-tlwg podman diodon htop rsync lm-sensors stress google-chrome-stable
+apt-get -y install i3 xorg xinit lightdm thunar neovim git xrdp xfce4-terminal fonts-thai-tlwg podman diodon htop rsync lm-sensors stress google-chrome-stable autossh tmux
 
 # enabling i3 desktop
 systemctl enable lightdm
