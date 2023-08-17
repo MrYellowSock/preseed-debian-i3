a9e83355cd7f6644078be9c13f93e32681e2c712
MrYellowSock <hahaunji@gmail.com>
2023-08-17 20:29:39 +0700

    ok

--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # I3 + essentials installation
 apt-get update
+apt-get -y install gnupg
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 apt-get update
-apt-get -y install i3 xorg xinit lightdm thunar neovim git xrdp xfce4-terminal fonts-thai-tlwg podman diodon htop rsync lm-sensors stress google-chrome-stable autossh tmux kitty
+apt-get -y install i3 xorg xinit lightdm thunar xrdp xfce4-terminal
+apt-get -y install neovim git fonts-thai-tlwg podman diodon htop rsync lm-sensors stress google-chrome-stable autossh tmux kitty ufw
 
 # TODO : Set Maxclients in /etc/x11/xrdp/xorg.conf to >= 512
 
