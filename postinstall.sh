cefa9e356fb47922841d84be26c6d85e7e60b25a
MrYellowSock <hahaunji@gmail.com>
2024-02-17 14:04:16 +0700

    add ntp

--- postinstall.sh
+++ postinstall.sh
@@ -5,9 +5,10 @@ apt-get -y install gnupg
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 apt-get update
-apt-get -y install i3 xorg xinit lightdm thunar xrdp xfce4-terminal nfs-common portmap
+apt-get -y install i3 xorg xinit lightdm thunar xrdp xfce4-terminal nfs-common portmap systemd-timesyncd
 apt-get -y install neovim git fonts-thai-tlwg podman diodon htop rsync lm-sensors stress google-chrome-stable autossh tmux kitty ufw sshpass
 
+timedatectl set-ntp true
 # TODO : Set Maxclients in /etc/x11/xrdp/xorg.conf to >= 512
 
 # enabling i3 desktop
