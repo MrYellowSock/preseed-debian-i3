32ac9629e9365b87797310c2ab6f3e9153c01a3d
MrYellowSock <hahaunji@gmail.com>
2024-04-14 11:36:53 +0700

    wget fix

--- postinstall.sh
+++ postinstall.sh
@@ -5,7 +5,7 @@ apt-get -y install gnupg
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 apt-get update
-apt-get -y install i3 xorg xinit lightdm thunar xrdp xfce4-terminal nfs-common portmap systemd-timesyncd libssl3
+apt-get -y install i3 xorg xinit lightdm thunar xrdp xfce4-terminal nfs-common portmap systemd-timesyncd libssl3 curl
 apt-get -y install neovim git fonts-thai-tlwg podman diodon htop rsync lm-sensors stress google-chrome-stable autossh tmux kitty ufw sshpass
 
 timedatectl set-ntp true
